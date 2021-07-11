fe5f979accd4241ac5b88984897c71da792ffd3d
zomdar <zomdar@gmail.com>
2021-07-10 19:25:34 -0500

    adding stuff

---
+++ src/mapping.ts
@@ -0,0 +1,40 @@
+import {
+  TokenURIUpdated as TokenURIUpdatedEvent,
+  Transfer as TransferEvent,
+  Token as TokenContract
+} from "../generated/Token/Token"
+
+import {
+  Token, User
+} from '../generated/schema'
+
+export function handleTokenURIUpdated(event: TokenURIUpdatedEvent): void {
+  let token = Token.load(event.params._tokenId.toString());
+  token.contentURI = event.params._uri;
+  token.save();
+}
+
+// update the handleTransfer function to add the createdAtTimestamp to the token object
+export function handleTransfer(event: TransferEvent): void {
+  let token = Token.load(event.params.tokenId.toString());
+  if (!token) {
+    token = new Token(event.params.tokenId.toString());
+    token.creator = event.params.to.toHexString();
+    token.tokenID = event.params.tokenId;
+    // Add the createdAtTimestamp to the token object
+    token.createdAtTimestamp = event.block.timestamp;
+
+    let tokenContract = TokenContract.bind(event.address);
+    token.contentURI = tokenContract.tokenURI(event.params.tokenId);
+    token.metadataURI = tokenContract.tokenMetadataURI(event.params.tokenId);
+  }
+  token.owner = event.params.to.toHexString();
+  token.save();
+
+  let user = User.load(event.params.to.toHexString());
+  if (!user) {
+    user = new User(event.params.to.toHexString());
+    user.save();
+  }
+}
+
